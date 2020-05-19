503b55a27254c8f6ca31ec10140aa3889fea87c1
EC2 Default User <ec2-user@ip-172-31-84-107.ec2.internal>
2020-05-19 17:14:05 +0000

    Creating methods to activate add to basket button

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ const productsDOM = document.querySelector('.products-center');
 
 let cart = [];
 
+// Array for buttons 
+
+let buttonsDOM = [];
+
 // Getting the products using the class function, locally from products.json, with properties assigned inside a constructor() method
 
 class Products {
@@ -34,24 +38,123 @@ class Products {
         try {
             let result = await fetch('products.json');
             let data = await result.json(); 
-            return data;
-            
+            let products = data.items; 
+            products = products.map(item =>{
+                const {title,price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url; 
+                
+                return {title,price,id,image}
+            })
+            return products;
         } catch (error) {
             console.log(error); 
             }
     }
 }
 
+
+
+
+
 // User interface class to DISPLAY products, getting items that are being returned from Products and displaying them
 
 class UI {
     
+    
+    displayProducts(products){
+       let result = ""; 
+       //Using array method to loop over product array
+       products.forEach(product => {
+           //Use template literals to allow embedded expressions and get single product from html (after commenting it out)
+           result +=`
+           <!-- Single Product -->
+            
+            <article class="single-product">
+                
+                <div class="img-container">
+                    
+                    <img 
+                    src=${product.image}
+                    alt="product" 
+                    class="product-img"></img>
+                    
+                    <button class="basket-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-basket"></i>
+                        add to basket
+                    </button>
+                    
+                </div>
+                
+                <h3>${product.title}</h3>
+                
+                <h4>£${product.price}</h4>
+                
+            </article>
+        
+            <!-- End of Single Product -->
+           
+           `
+       })
+       productsDOM.innerHTML= result;
+    }
+    
+    getBasketButtons(){
+        
+        //Using spread operator
+        const buttons = [...document.querySelectorAll(".basket-btn")];
+        
+        buttonsDOM = buttons;
+        
+        buttons.forEach(button =>{
+            let id = button.dataset.id;
+            //Setup the inCart variable, use find method for items in shopping basket
+            let inCart = cart.find(item => item.id === id);
+            //If the item is in the basket, this what I want to do
+            if(inCart){
+                //Use InnerText property to return a desired text content of the specified node, and all its descendants
+                button.innerText = "In Basket";
+                button.disabled = true;
+            }
+            
+                button.addEventListener("click", (event)=>{
+                    event.target.innerText = "In Basket";
+                    event.target.disabled = true;
+                    //Get prodcut from products, based on the id
+                    let cartItem = Storage.getProduct(id);
+                    console.log(cartItem); 
+                    //Add product to the basket
+                    //Save cart in local storage 
+                    //Set cart values 
+                    //Display cart item
+                    //Show the cart
+                })
+        })
+    }
+    
 }
+
+
+
+
+
+
 
 // Local storage class 
 
 class Storage {
     
+    //Creating a static method to save products 
+    static saveProducts(products){
+        //setItem() method sets the value of the specified Storage Object item
+        localStorage.setItem("products", JSON.stringify(products));
+    }
+    
+    //create static method, method name getProduct, with id as an argument. Products is the variable, I am parsing JSON because it's stored as a string
+    static getProduct(id){
+        let products = JSON.parse(localStorage.getItem(localStorage.getItem("products"));
+        return products.find(product => product.id === id);
+    }
 }
 
 // The HTML DOM EventListener, within instances are created, to call the functions
@@ -64,6 +167,17 @@ document.addEventListener("DOMContentLoaded", ()=>{
     
     // Get all products 
     
-    products.getProducts().then(data => console.log(data)); 
+    products.getProducts().then(products => {
+        
+    ui.displayProducts(products);
+    //class used to call the saveProducts static method to add products to Local Storage
+    Storage.saveProducts(products);
+    
+    }).then(()=>{
+        
+        ui.getBasketButtons(); 
+        
+    });
+    
     
 });
